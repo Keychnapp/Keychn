ffb60049bb222dae664823aed240b8deb93c254b
Rohit Kumar <jarvistech@Rohits-MacBook-Air.local>
2018-01-10 21:24:14 +0530

    Masterclass Host End UI updated.

---
+++ Keychn/SourceClasses/TriangleView/TriLabelView.swift
@@ -0,0 +1,21 @@
+//
+//  TriLabelView.swift
+//  Keychn
+//
+//  Created by Rohit Kumar on 10/01/2018.
+//  Copyright © 2018 Keychn Experience. All rights reserved.
+//
+
+import UIKit
+
+class TriLabelView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
